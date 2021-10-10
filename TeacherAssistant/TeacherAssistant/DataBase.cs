6913e521f1c30fa2993cf87eb855a860d280fc59
Partho Debnath <parthodebnath28@gmail.com>
2021-10-10 13:20:12 +0600

    Admin Login

---
+++ TeacherAssistant/TeacherAssistant/DataBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TeacherAssistant
+{
+    static class DataBase
+    {
+        public static string Connect_String()
+        {
+            string database = "datasource=localhost; username=root; password=; port=3306; sslmode=none;" +
+                   "database=teacher_assistant;";
+
+            return database;
+        }
+
+    }
+}
---
+++ TeacherAssistant/TeacherAssistant/Login Form.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace TeacherAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.login_As = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Registration = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Button();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // login_As
+            // 
+            this.login_As.FormattingEnabled = true;
+            this.login_As.Location = new System.Drawing.Point(249, 95);
+            this.login_As.Name = "login_As";
+            this.login_As.Size = new System.Drawing.Size(196, 21);
+            this.login_As.TabIndex = 1;
+            this.login_As.SelectedIndexChanged += new System.EventHandler(this.login_As_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(244, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "E-mail:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(244, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password:";
+            // 
+            // Registration
+            // 
+            this.Registration.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registration.Location = new System.Drawing.Point(207, 371);
+            this.Registration.Name = "Registration";
+            this.Registration.Size = new System.Drawing.Size(282, 42);
+            this.Registration.TabIndex = 7;
+            this.Registration.Text = "Don\'t have any account?";
+            this.Registration.UseVisualStyleBackColor = true;
+            this.Registration.Click += new System.EventHandler(this.Registration_Click);
+            // 
+            // Login
+            // 
+            this.Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.Location = new System.Drawing.Point(274, 290);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(91, 40);
+            this.Login.TabIndex = 8;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(249, 189);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(190, 20);
+            this.Email.TabIndex = 9;
+            this.Email.TextChanged += new System.EventHandler(this.Email_TextChanged);
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(236, 264);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(190, 20);
+            this.Password.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(617, 503);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Registration);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.login_As);
+            this.Controls.Add(this.label1);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox login_As;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Registration;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.TextBox Password;
+    }
+}
+
