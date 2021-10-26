e15f6706cf38e5fdcf4889a52430e7a561ac5a03
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-27 03:08:21 +0600

    Sign In Form and Admin Profile Design

--- TeacherAssistant/TeacherAssistant/AdminProfile.Designer.cs
+++ TeacherAssistant/TeacherAssistant/AdminProfile.Designer.cs
@@ -29,26 +29,38 @@ namespace TeacherAssistant
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin_Profile));
             this.AddDept = new System.Windows.Forms.Button();
             this.Add_New_Student = new System.Windows.Forms.Button();
             this.Instructor_Registration_Permission = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // AddDept
             // 
-            this.AddDept.Location = new System.Drawing.Point(243, 32);
+            this.AddDept.Font = new System.Drawing.Font("MS Reference Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddDept.Location = new System.Drawing.Point(465, 13);
             this.AddDept.Name = "AddDept";
-            this.AddDept.Size = new System.Drawing.Size(203, 73);
+            this.AddDept.Size = new System.Drawing.Size(400, 73);
             this.AddDept.TabIndex = 0;
-            this.AddDept.Text = "Add Department Name";
+            this.AddDept.Text = "Add Department ";
             this.AddDept.UseVisualStyleBackColor = true;
             this.AddDept.Click += new System.EventHandler(this.AddDept_Click);
             // 
             // Add_New_Student
             // 
-            this.Add_New_Student.Location = new System.Drawing.Point(243, 126);
+            this.Add_New_Student.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add_New_Student.Location = new System.Drawing.Point(465, 13);
             this.Add_New_Student.Name = "Add_New_Student";
-            this.Add_New_Student.Size = new System.Drawing.Size(203, 73);
+            this.Add_New_Student.Size = new System.Drawing.Size(400, 73);
             this.Add_New_Student.TabIndex = 1;
             this.Add_New_Student.Text = "Add Student";
             this.Add_New_Student.UseVisualStyleBackColor = true;
@@ -56,25 +68,86 @@ namespace TeacherAssistant
             // 
             // Instructor_Registration_Permission
             // 
-            this.Instructor_Registration_Permission.Location = new System.Drawing.Point(243, 218);
+            this.Instructor_Registration_Permission.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Instructor_Registration_Permission.Location = new System.Drawing.Point(465, 13);
             this.Instructor_Registration_Permission.Name = "Instructor_Registration_Permission";
-            this.Instructor_Registration_Permission.Size = new System.Drawing.Size(203, 73);
+            this.Instructor_Registration_Permission.Size = new System.Drawing.Size(400, 73);
             this.Instructor_Registration_Permission.TabIndex = 2;
             this.Instructor_Registration_Permission.Text = "Instructor Registration Permission";
             this.Instructor_Registration_Permission.UseVisualStyleBackColor = true;
             this.Instructor_Registration_Permission.Click += new System.EventHandler(this.Instructor_Registration_Permission_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.AddDept);
+            this.panel1.Location = new System.Drawing.Point(-2, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(966, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Salmon;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Controls.Add(this.Add_New_Student);
+            this.panel2.Location = new System.Drawing.Point(-2, 228);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(966, 100);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Coral;
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.Controls.Add(this.Instructor_Registration_Permission);
+            this.panel3.Location = new System.Drawing.Point(-2, 363);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(966, 100);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Location = new System.Drawing.Point(71, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(303, 587);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel5.BackgroundImage")));
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Location = new System.Drawing.Point(36, 132);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(233, 270);
+            this.panel5.TabIndex = 0;
+            // 
             // Admin_Profile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Instructor_Registration_Permission);
-            this.Controls.Add(this.Add_New_Student);
-            this.Controls.Add(this.AddDept);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(962, 587);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Admin_Profile";
             this.Text = "Admin Profile";
             this.Load += new System.EventHandler(this.Admin_Profile_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -84,5 +157,10 @@ namespace TeacherAssistant
         private System.Windows.Forms.Button AddDept;
         private System.Windows.Forms.Button Add_New_Student;
         private System.Windows.Forms.Button Instructor_Registration_Permission;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
     }
 }
--- TeacherAssistant/TeacherAssistant/Login Form.Designer.cs
+++ TeacherAssistant/TeacherAssistant/Login Form.Designer.cs
@@ -29,41 +29,51 @@ namespace TeacherAssistant
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.login_As = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.Registration = new System.Windows.Forms.Button();
             this.Login = new System.Windows.Forms.Button();
             this.Email = new System.Windows.Forms.TextBox();
             this.Password = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(294, 23);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(502, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 39);
+            this.label1.Size = new System.Drawing.Size(196, 58);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Login";
+            this.label1.Text = "Sign In";
             // 
             // login_As
             // 
             this.login_As.FormattingEnabled = true;
-            this.login_As.Location = new System.Drawing.Point(249, 95);
+            this.login_As.Location = new System.Drawing.Point(480, 173);
             this.login_As.Name = "login_As";
-            this.login_As.Size = new System.Drawing.Size(196, 21);
+            this.login_As.Size = new System.Drawing.Size(228, 21);
             this.login_As.TabIndex = 1;
             this.login_As.SelectedIndexChanged += new System.EventHandler(this.login_As_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(244, 151);
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(553, 212);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 25);
             this.label2.TabIndex = 4;
@@ -72,71 +82,147 @@ namespace TeacherAssistant
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(244, 236);
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(540, 295);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(121, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Password:";
             // 
-            // Registration
-            // 
-            this.Registration.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Registration.Location = new System.Drawing.Point(207, 371);
-            this.Registration.Name = "Registration";
-            this.Registration.Size = new System.Drawing.Size(282, 42);
-            this.Registration.TabIndex = 7;
-            this.Registration.Text = "Don\'t have any account?";
-            this.Registration.UseVisualStyleBackColor = true;
-            this.Registration.Click += new System.EventHandler(this.Registration_Click);
-            // 
             // Login
             // 
+            this.Login.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Login.Location = new System.Drawing.Point(274, 290);
+            this.Login.Location = new System.Drawing.Point(522, 392);
             this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(91, 40);
+            this.Login.Size = new System.Drawing.Size(150, 37);
             this.Login.TabIndex = 8;
             this.Login.Text = "Login";
-            this.Login.UseVisualStyleBackColor = true;
+            this.Login.UseVisualStyleBackColor = false;
             this.Login.Click += new System.EventHandler(this.Login_Click);
             // 
             // Email
             // 
-            this.Email.Location = new System.Drawing.Point(249, 189);
+            this.Email.Location = new System.Drawing.Point(480, 251);
             this.Email.Name = "Email";
-            this.Email.Size = new System.Drawing.Size(190, 20);
+            this.Email.Size = new System.Drawing.Size(228, 20);
             this.Email.TabIndex = 9;
             this.Email.TextChanged += new System.EventHandler(this.Email_TextChanged);
             // 
             // Password
             // 
-            this.Password.Location = new System.Drawing.Point(236, 264);
+            this.Password.Location = new System.Drawing.Point(480, 338);
             this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(190, 20);
+            this.Password.Size = new System.Drawing.Size(228, 20);
             this.Password.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-7, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 614);
+            this.panel1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Beige;
+            this.label6.Location = new System.Drawing.Point(19, 347);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(235, 27);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Teacher Assistant";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Beige;
+            this.label5.Location = new System.Drawing.Point(109, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 27);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "To";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Beige;
+            this.label4.Location = new System.Drawing.Point(76, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 27);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Welcome";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(56, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(162, 182);
+            this.panel2.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label7.Location = new System.Drawing.Point(534, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "User Type:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(617, 503);
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1008, 607);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.Password);
             this.Controls.Add(this.Email);
             this.Controls.Add(this.Login);
-            this.Controls.Add(this.Registration);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.login_As);
             this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Login Form";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,10 +234,15 @@ namespace TeacherAssistant
         private System.Windows.Forms.ComboBox login_As;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button Registration;
         private System.Windows.Forms.Button Login;
         private System.Windows.Forms.TextBox Email;
         private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
     }
 }
 
